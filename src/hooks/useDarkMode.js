0dfc145024f82abc40e15603a8c470b171524524
happy-crystal-2002 <crystal.best.2002@gmail.com>
2025-02-04 11:35:28 -0800

    fix: disable darkmode

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -7,25 +7,25 @@ export default function useDarkMode() {
   const [darkModeEnabled, setDarkModeEnabled] = useState(true);
 
   useEffect(() => {
-    if (window.localStorage.getItem('theme-mode') === 'light') {
-      document.documentElement.classList.remove('dark');
-      setDarkModeEnabled(false);
-    } else {
-      document.documentElement.classList.add('dark');
-      setDarkModeEnabled(true);
-    }
+    // if (window.localStorage.getItem('theme-mode') === 'light') {
+    //   document.documentElement.classList.remove('dark');
+    //   setDarkModeEnabled(false);
+    // } else {
+    //   document.documentElement.classList.add('dark');
+    //   setDarkModeEnabled(true);
+    // }
   }, []);
 
   const toggleMode = () => {
-    if (darkModeEnabled) {
-      setDarkModeEnabled(false);
-      document.documentElement.classList.remove('dark');
-      localStorage.setItem('theme-mode', 'light');
-    } else {
-      setDarkModeEnabled(true);
-      document.documentElement.classList.add('dark');
-      localStorage.setItem('theme-mode', 'dark');
-    }
+    // if (darkModeEnabled) {
+    //   setDarkModeEnabled(false);
+    //   document.documentElement.classList.remove('dark');
+    //   localStorage.setItem('theme-mode', 'light');
+    // } else {
+    //   setDarkModeEnabled(true);
+    //   document.documentElement.classList.add('dark');
+    //   localStorage.setItem('theme-mode', 'dark');
+    // }
   };
 
   return {
